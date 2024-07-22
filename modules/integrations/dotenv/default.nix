f91d29d0794fde03a6975bb0c3f6680650f32bfe
Gregory Lifhits <gregory.lifhits@assaabloy.com>
2024-07-22 10:45:42 -0400

    refactor(dotenv): streamline parsing and path definition in dotenv integration module

--- modules/integrations/dotenv/default.nix
+++ modules/integrations/dotenv/default.nix
@@ -10,7 +10,6 @@
     options.perSystem = flake-parts-lib.mkPerSystemOption ({
       lib,
       config,
-      self',
       ...
     }: let
       inherit (lib) types mkOption;
@@ -22,32 +21,20 @@
         then filenames
         else [filenames];
       dotenvFiles = normalizeFilenames cfg.filename;
-      dotenvPaths = map (filename: (config.snow-blower.paths.root + "/" + filename)) dotenvFiles;
+      dotenvPaths = map (filename: ("${config.snow-blower.paths.root}" + "/" + filename)) dotenvFiles;
 
       # Parses a single line of an environment file.
-      parseLine = line:
-        let
-          # Attempt to match the line to the pattern for an env variable declaration.
-          # Supports optional export keyword, captures variable names and values.
-          parts = builtins.match "^[[:space:]]*(export[[:space:]]+)?([^[:space:]=#]+)[[:space:]]*=[[:space:]]*(.*)[[:space:]]*$" line;
-        in
-        if parts != null && builtins.length parts == 4
-        then
-          # If a match is found and has the correct number of parts, create an attribute set.
-          { name = parts[2]; value = parts[3]; }
-        else
-          # Return null if no valid match is found.
-          null;
+      parseLine = line: let
+        parts = builtins.match "([[:space:]]*export[[:space:]]+)?([^[:space:]=#]+)[[:space:]]*=[[:space:]]*(.*)" line;
+      in
+        if parts != null && builtins.length parts == 3
+        then {
+          name = builtins.elemAt parts 1;
+          value = builtins.elemAt parts 2;
+        }
+        else null;
 
-      # Parses the entire content of a .env file into a set of Nix attributes.
-      parseEnvFile = content:
-        builtins.listToAttrs (
-          # Use lib.filter to remove any null results from unsuccessful line parses.
-          lib.filter (x: x != null) (
-            # Map parseLine over each line split from the input content.
-            map parseLine (lib.splitString "\n" content)
-          )
-        );
+      parseEnvFile = content: builtins.listToAttrs (lib.filter (x: !builtins.isNull x) (map parseLine (lib.splitString "\n" content)));
 
       mergeEnvFiles = files:
         lib.foldl' (acc: file:
