5d08eb328512ae2f812c9560d7ebd882714eed8d
Your Name <you@example.com>
2025-06-14 17:08:12 -0400

    refactor: move code quality program modules out of programs directory

--- modules/code-quality/default.nix
+++ modules/code-quality/default.nix
@@ -1,13 +1,13 @@
 {
   imports = [
     # The Code Quality tools avliable.
-    ./programs/alejandra.nix
-    ./programs/biome.nix
-    ./programs/deadnix.nix
-    ./programs/keep-sorted.nix
-    ./programs/mdformat.nix
-    ./programs/nixfmt.nix
-    ./programs/ruff.nix
-    ./programs/statix.nix
+    ./alejandra.nix
+    ./biome.nix
+    ./deadnix.nix
+    ./keep-sorted.nix
+    ./mdformat.nix
+    ./nixfmt.nix
+    ./ruff.nix
+    ./statix.nix
   ];
 }
